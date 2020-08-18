0f7b508beec578fefdafb68fe9730c697e0eac6c
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-18 06:47:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,15 +21,3 @@ end
 def shift_with_args(array)
   array.shift[2]
 end
-
-#pop_with_args
-  takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them (FAILED - 1)
-  decreases the length of the array by 2 (FAILED - 2)
-
-using_shift
-  takes in an argument of an array and uses the shift method to remove the first item and return it (FAILED - 3)
-  decreases the length of the array by 1 (FAILED - 4)
-
-shift_with_args
-  takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array (FAILED - 5)
-  decreases the length of the array by 2 (FAILED - 6)
